f99822583a59ee9f6d0fcf0a82970f41779b72aa
olivax28 <olivax28@outlook.com>
2026-02-01 00:42:28 -0500

    did a little more work on the tutorial

---
+++ week02/tutorial/Assets/ObjectFallController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ObjectFallController : MonoBehaviour
+{
+    float wait = 0.1f;
+    public GameObject fallingObject;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("Fall",wait,wait);
+    }
+
+    // Update is called once per frame
+    void Fall()
+    {
+        Instantiate(fallingObject, new Vector3(Random.Range(-10,10),10,0), Quaternion.identity);
+    }
+}
